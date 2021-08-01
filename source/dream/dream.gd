b60be81e3748ae1fb25d8d6df00f4d0e54b83198
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-08-01 14:26:44 +0300

    procedural generation fail

--- source/dream/dream.gd
+++ source/dream/dream.gd
@@ -1,10 +1,16 @@
 extends Spatial
 
 onready var audio = $audio
+onready var player = preload("res://player/player.tscn")
 
 func _ready():
 	globals._dream_rng_refresh()
-	_load_layout()
+	#_load_layout()
+	_spawn_player()
+
+func _spawn_player():
+	var new_player = player.instance()
+	add_child(new_player)
 
 func _load_layout():
 	var layout_id = globals.dream_rng.randi()%globals.layouts.size()
