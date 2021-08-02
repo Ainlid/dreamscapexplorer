990ce7a064461a449b34e0cc5691f40ee17bac22
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-08-02 21:56:29 +0300

    metallic/roughness

--- source/dream_gen/layouts/layout.gd
+++ source/dream_gen/layouts/layout.gd
@@ -13,10 +13,14 @@ func _set_materials():
 		new_mat.albedo_color.r = globals.dream_rng.randf()
 		new_mat.albedo_color.g = globals.dream_rng.randf()
 		new_mat.albedo_color.b = globals.dream_rng.randf()
-		new_mat.uv1_scale = Vector3.ONE * globals.dream_rng.randf_range(10.0, 100.0)
+		new_mat.uv1_scale = Vector3.ONE * globals.dream_rng.randf_range(10.0, 50.0)
 		var normal_id = globals.dream_rng.randi()%globals.normalmaps.size()
 		new_mat.normal_enabled = true
 		new_mat.normal_texture = globals.normalmaps[normal_id]
-		#new_mat.metallic = globals.dream_rng.randf()
-		#new_mat.roughness = globals.dream_rng.randf()
+		var metallic_chance = globals.dream_rng.randf()
+		if metallic_chance > 0.5:
+			new_mat.metallic = globals.dream_rng.randf_range(0.5, 1.0)
+		var rough_chance = globals.dream_rng.randf()
+		if rough_chance > 0.5:
+			new_mat.roughness = globals.dream_rng.randf_range(0.2, 0.5)
 		mesh.set_surface_material(n_mat, new_mat)
