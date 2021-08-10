4bf9db2fbdbb04e2ed2ab6fccd6f7d721b93f355
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-08-10 15:47:42 +0300

    world gen improvement

--- source/dream_gen/dream_gen.gd
+++ source/dream_gen/dream_gen.gd
@@ -8,15 +8,14 @@ onready var sun = $sun
 
 var grid_size = 8
 var cell_distance = 40.0
-var floors_array = []
 
-var prop_array = []
+onready var floor_tile = preload("res://dream_gen/tiles/floors/floor.tscn")
 
-onready var floor_flat = preload("res://dream_gen/tiles/floors/test_flat.tscn")
-onready var floor_stairs = preload("res://dream_gen/tiles/floors/test_stairs.tscn")
+onready var frame_flat = preload("res://dream_gen/tiles/frames/frame_flat.tscn")
+onready var frame_stairs = preload("res://dream_gen/tiles/frames/frame_stairs.tscn")
 
-onready var wall_indoor = preload("res://dream_gen/tiles/walls/test_wall_indoor.tscn")
-onready var wall_outdoor = preload("res://dream_gen/tiles/walls/test_wall_outdoor.tscn")
+onready var wall_indoor = preload("res://dream_gen/tiles/walls/wall_indoor.tscn")
+onready var wall_outdoor = preload("res://dream_gen/tiles/walls/wall_outdoor.tscn")
 
 func _ready():
 	_generate()
@@ -26,7 +25,6 @@ func _generate():
 	_set_env()
 	_set_sun()
 	_spawn_grid()
-	_spawn_player()
 
 func _spawn_grid():
 	var offset_h = -(grid_size - 1.0) / 2.0 * cell_distance
@@ -35,7 +33,11 @@ func _spawn_grid():
 	var y_levels = 1
 	if globals.dream_rng.randf() > 0.5:
 		y_levels = globals.dream_rng.randi_range(2, grid_size)
-	var has_walls = globals.dream_rng.randf() > 0.5
+	var frame_type_bias = globals.dream_rng.randf_range(0.1, 0.9)
+	var wall_bias = globals.dream_rng.randf_range(0.1, 0.9)
+	var wall_type_bias = globals.dream_rng.randf_range(0.1, 0.9)
+	var player_spawn_frame = globals.dream_rng.randi()%(grid_size * y_levels * grid_size)
+	var frame_number = 0
 	for n_x in grid_size:
 		for n_y in y_levels:
 			for n_z in grid_size:
@@ -43,33 +45,35 @@ func _spawn_grid():
 				coords.x = n_x * cell_distance
 				coords.y = n_y * cell_distance / 2.0
 				coords.z = n_z * cell_distance
-				var spawn_chance = globals.dream_rng.randf() > 0.5
-				if spawn_chance:
-					var new_floor
-					if globals.dream_rng.randf() > 0.5 and n_y > 0:
-						new_floor = floor_stairs.instance()
-					else:
-						new_floor = floor_flat.instance()
-					grid.add_child(new_floor)
-					new_floor.translation = coords
-					new_floor.rotation.y = globals.dream_rng.randi()%4 * PI/2.0
-					floors_array.append(new_floor)
+				#add a frame
+				var new_frame
+				if globals.dream_rng.randf() > frame_type_bias and n_y > 0:
+					new_frame = frame_stairs.instance()
+				else:
+					new_frame = frame_flat.instance()
+				grid.add_child(new_frame)
+				new_frame.translation = coords
+				new_frame.rotation.y = globals.dream_rng.randi()%4 * PI/2.0
+				if frame_number == player_spawn_frame:
+					var new_player = player.instance()
+					grid.add_child(new_player)
+					var player_offset = Vector3(0.0, 2.0, 20.0)
+					new_player.translation = new_frame.translation + player_offset
+					new_player.rotation.y = globals.dream_rng.randi()%4 * PI/2.0
+					new_player.bound = grid_size / 2.0 * cell_distance
+				frame_number += 1
+				#add a floor or a wall
+				if globals.dream_rng.randf() > wall_bias:
+					var new_floor = floor_tile.instance()
+					new_frame.add_child(new_floor)
 				else:
 					var new_wall
-					if has_walls:
+					if globals.dream_rng.randf() > wall_type_bias:
 						new_wall = wall_indoor.instance()
 					else:
 						new_wall = wall_outdoor.instance()
 					grid.add_child(new_wall)
 					new_wall.translation = coords
-
-func _spawn_player():
-	var spawn_floor_id = globals.dream_rng.randi()%floors_array.size()
-	var spawn_floor = floors_array[spawn_floor_id]
-	var new_player = player.instance()
-	grid.add_child(new_player)
-	new_player.translation = spawn_floor.translation + Vector3.UP * 2.0
-	new_player.bound = grid_size / 2.0 * cell_distance
 
 func _randomize_mats():
 	for n in globals.materials.size():
