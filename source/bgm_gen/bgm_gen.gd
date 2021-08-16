635a1577f03c353e4513c0071833eb61395434e9
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-08-16 15:57:32 +0300

    old music algorithm with a few changes

--- source/bgm_gen/bgm_gen.gd
+++ source/bgm_gen/bgm_gen.gd
@@ -1,65 +1,75 @@
 extends Node
 
-onready var players = [$audio1, $audio2, $audio3, $audio4]
+onready var melody = $melody
+onready var bass = $bass
 
 onready var timer =$timer
 var tempo = 120.0
 
-var seq1 = []
-var seq2 = []
-var seq3 = []
-var seq4 = []
+var note_scale = []
+var transposition = 0.0
+
+var melody_note_seq = []
+var melody_play_seq = []
+var bass_note_seq = []
+var bass_play_seq = []
 
 var current_note = 0
 
 func _ready():
 	_bgm_start()
-	_set_pitches()
 
 func _bgm_start():
 	_set_samples()
+	_make_scale()
 	_sequence()
 	tempo = globals.dream_rng.randf_range(100.0, 180.0)
 	timer.wait_time = 60.0/tempo
 	timer.start()
 
 func _set_samples():
-	for n in 4:
-		var sample_id = globals.dream_rng.randi()%globals.samples.size()
-		var sample = globals.samples[sample_id]
-		players[n].stream = sample
+	var melody_sample_id = globals.dream_rng.randi()%globals.samples.size()
+	var melody_sample = globals.samples[melody_sample_id]
+	melody.stream = melody_sample
+	var bass_sample_id = globals.dream_rng.randi()%globals.samples.size()
+	var bass_sample = globals.samples[bass_sample_id]
+	bass.stream = bass_sample
 
-func _set_pitches():
-	var note_scale
+func _make_scale():
 	if globals.dream_rng.randf() > 0.5:
 		#major scale
 		note_scale = [0, 2, 4, 5, 7, 9, 11]
 	else:
 		#minor scale
 		note_scale = [0, 2, 3, 5, 7, 8, 10]
-	var transposition = globals.dream_rng.randi_range(0, 11)
-	for n in 4:
-		var note_id = globals.dream_rng.randi()%note_scale.size()
-		players[n].pitch_scale = _note_to_pitch(note_scale[note_id] + transposition)
+	transposition = globals.dream_rng.randi_range(0, 11)
 
 func _sequence():
-	for n_seq1 in 16:
-		seq1.append(n_seq1%4 == 1)
-	for n_seq2 in 16:
-		seq2.append(globals.dream_rng.randf() > 0.5 and n_seq2%2 == 1)
-	for n_seq3 in 16:
-		seq3.append(globals.dream_rng.randf() > 0.5)
-	for n_seq4 in 16:
-		seq4.append(globals.dream_rng.randf() > 0.5)
+	for melody_notes in 16:
+		var note_id = globals.dream_rng.randi()%7
+		melody_note_seq.append(note_scale[note_id])
+	for melody_plays in 16:
+		melody_play_seq.append(globals.dream_rng.randf() > 0.5)
+	for bass_notes in 16:
+		var note_id = globals.dream_rng.randi()%7
+		bass_note_seq.append(note_scale[note_id])
+	for bass_plays in 16:
+		if bass_plays%4 == 1:
+			bass_play_seq.append(true)
+		else:
+			bass_play_seq.append(globals.dream_rng.randf() > 0.5 and bass_plays%2 == 1)
 
 func _note_to_pitch(note_value):
 	return pow(2.0, note_value/12.0)
 
 func _timeout():
-	var seqs = [seq1, seq2, seq3, seq4]
-	for n in 4:
-		if seqs[n][current_note]:
-			players[n].play()
+	#melody and bass
+	melody.pitch_scale = _note_to_pitch(melody_note_seq[current_note] + transposition)
+	if melody_play_seq[current_note]:
+		melody.play()
+	bass.pitch_scale = _note_to_pitch(bass_note_seq[current_note] + transposition - 12.0)
+	if bass_play_seq[current_note]:
+		bass.play()
 	#loop
 	if current_note < 15:
 		current_note += 1
