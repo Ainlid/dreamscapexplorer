55251a237f937e28eaa2779427a04a3447902b6f
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-08-31 15:37:46 +0300

    Revert "I'm not sure"
    This reverts commit 8f71ca3e2c6c5b319de3efd815397406dfc2fd8d.

--- source/globals/globals.gd
+++ source/globals/globals.gd
@@ -19,7 +19,35 @@ preload("res://materials/tile.tres"),
 preload("res://materials/tree1.tres"),
 preload("res://materials/tree2.tres")]
 
-const textures = [preload("res://textures/albedo/glass.png")]
+const textures = [preload("res://textures/source/bark.png"),
+preload("res://textures/source/brick.png"),
+preload("res://textures/source/ceramic_tiles.png"),
+preload("res://textures/source/chess.png"),
+preload("res://textures/source/cogs.png"),
+preload("res://textures/source/fabric.png"),
+preload("res://textures/source/foam.png"),
+preload("res://textures/source/glass.png"),
+preload("res://textures/source/glass_blocks.png"),
+preload("res://textures/source/grass.png"),
+preload("res://textures/source/ice.png"),
+preload("res://textures/source/leaf.png"),
+preload("res://textures/source/leaves.png"),
+preload("res://textures/source/marble.png"),
+preload("res://textures/source/meat.png"),
+preload("res://textures/source/metal.png"),
+preload("res://textures/source/paint.png"),
+preload("res://textures/source/pipes.png"),
+preload("res://textures/source/planks.png"),
+preload("res://textures/source/quartz.png"),
+preload("res://textures/source/rug.png"),
+preload("res://textures/source/snow.png"),
+preload("res://textures/source/soap_film.png"),
+preload("res://textures/source/sponge.png"),
+preload("res://textures/source/stone.png"),
+preload("res://textures/source/textbook.png"),
+preload("res://textures/source/toy_bricks.png"),
+preload("res://textures/source/wall.png"),
+preload("res://textures/source/water.png")]
 
 var dream_rng
 
