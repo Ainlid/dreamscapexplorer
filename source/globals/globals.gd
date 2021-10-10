5d97603ffcf5db62235a90c16da76a375edbf83b
DiV4 <51695411+DiV4@users.noreply.github.com>
2021-10-10 14:54:33 +0300

    new assets and better material organization

--- source/globals/globals.gd
+++ source/globals/globals.gd
@@ -2,22 +2,21 @@ extends Node
 
 var tiles = [preload("res://dream_gen/tiles/flat.tscn"),
 preload("res://dream_gen/tiles/bridge1.tscn"),
-preload("res://dream_gen/tiles/street1.tscn")]
+preload("res://dream_gen/tiles/street1.tscn"),
+preload("res://dream_gen/tiles/forest.tscn")]
 
-var materials = [preload("res://materials/bridge1.tres"),
-preload("res://materials/bridge2.tres"),
-preload("res://materials/car1.tres"),
-preload("res://materials/car2.tres"),
-preload("res://materials/car3.tres"),
+var materials = [preload("res://materials/brick.tres"),
+preload("res://materials/brick2.tres"),
+preload("res://materials/concrete.tres"),
 preload("res://materials/floor.tres"),
-preload("res://materials/house1.tres"),
-preload("res://materials/house2.tres"),
-preload("res://materials/house3.tres"),
-preload("res://materials/street_lamp1.tres"),
-preload("res://materials/street_lamp2.tres"),
+preload("res://materials/glass.tres"),
+preload("res://materials/leaves.tres"),
+preload("res://materials/metal.tres"),
+preload("res://materials/rubber.tres"),
+preload("res://materials/stone.tres"),
 preload("res://materials/tile.tres"),
-preload("res://materials/tree1.tres"),
-preload("res://materials/tree2.tres")]
+preload("res://materials/wood.tres"),
+preload("res://materials/wood2.tres")]
 
 var dream_rng
 
